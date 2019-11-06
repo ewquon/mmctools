12b20391d0da86ade057befdbbd10c1e73ef21fd
Eliot Quon <eliot.quon@nrel.gov>
2019-11-06 15:19:29 -0700

    Store header records as a dictionary with keys as field names

--- measurements/little_r.py
+++ measurements/little_r.py
@@ -1,10 +1,67 @@
 """
 Helper functions for processing observational data in LITTLE_R observation format
 """
+from collections import OrderedDict
 
-# copied from table in from http://www2.mmm.ucar.edu/wrf/users/wrfda/OnlineTutorial/Help/littler.html
-# - can override prior to instantiating a Report
-header_record_str = 'F20.5        F20.5        A40        A40        A40        A40        F20.5        I10        I10        I10        I10        I10        L        L        L        I10        I10        A20        F13.5, I7        F13.5, I7        F13.5, I7        F13.5, I7        F13.5, I7        F13.5, I7        F13.5, I7        F13.5, I7        F13.5, I7        F13.5, I7        F13.5, I7        F13.5, I7        F13.5, I7        F13.5, I7'
+# from table in from http://www2.mmm.ucar.edu/wrf/users/wrfda/OnlineTutorial/Help/littler.html
+# - can modify prior to instantiating Report object
+header_records = OrderedDict([
+    ('Latitude', 'F20.5'),
+    ('Longitude', 'F20.5'),
+    ('ID', 'A40'),
+    ('Name', 'A40'),
+    ('Platform (FM‑Code)note', 'A40'),
+    ('Source', 'A40'),
+    ('Elevation', 'F20.5'),
+    ('Valid fields', 'I10'),
+    ('Num. errors', 'I10'),
+    ('Num. warnings', 'I10'),
+    ('Sequence number', 'I10'),
+    ('Num. duplicates', 'I10'),
+    ('Is sounding?', 'L'),
+    ('Is bogus?', 'L'),
+    ('Discard?', 'L'),
+    ('Unix time', 'I10'),
+    ('Julian day', 'I10'),
+    ('Date', 'A20'),
+    ('SLP', 'F13.5'),
+    ('SLP QC', 'I7'),
+    ('Ref Pressure', 'F13.5'),
+    ('Ref Pressure QC', 'I7'),
+    ('Ground Temp', 'F13.5'),
+    ('Ground Temp QC', 'I7'),
+    ('SST', 'F13.5'),
+    ('SST QC', 'I7'),
+    ('SFC Pressure', 'F13.5'),
+    ('SFC Pressure QC', 'I7'),
+    ('Precip', 'F13.5'),
+    ('Precip QC', 'I7'),
+    ('Daily Max T', 'F13.5'),
+    ('Daily Max T QC', 'I7'),
+    ('Daily Min T', 'F13.5'),
+    ('Daily Min T QC', 'I7'),
+    ('Night Min T', 'F13.5'),
+    ('Night Min T QC', 'I7'),
+    ('3hr Pres Change', 'F13.5'),
+    ('3hr Pres Change QC', 'I7'),
+    ('24hr Pres Change', 'F13.5'),
+    ('24hr Pres Change QC', 'I7'),
+    ('Cloud cover', 'F13.5'),
+    ('Cloud cover QC', 'I7'),
+    ('Ceiling', 'F13.5'),
+    ('Ceiling QC', 'I7'),
+#    ('Precipitable water', 'F13.5'),
+#    ('Precipitable water QC', 'I7'),
+])
+
+
+def boolean(s):
+    """Cast boolean string to python bool"""
+    s = s.strip().lower()
+    if (s == 't') or (s == 'true'):
+        return True
+    else:
+        return False
 
 
 class Report(object):
@@ -13,27 +70,33 @@ class Report(object):
     """
     def __init__(self,fname=None):
         # create header format string
-        header_records = header_record_str.replace(',',' ').split()
-        header_dtypes = []
-        for i,rec in enumerate(header_records):
+        self.header_dtypes = {}
+        self.header_fieldlen = {}
+        self.header_fmt = ''
+        for field,rec in header_records.items():
             dtype = rec[0].lower()
             if dtype == 'a':
                 # string
                 dtype = dtype.replace('a','s')
-                header_dtypes.append(str)
+                self.header_dtypes[field] = str
             elif dtype == 'l':
                 # boolean represented as T or F
                 dtype = dtype.replace('l','s')
-                header_dtypes.append(bool)
+                self.header_dtypes[field] = boolean
             elif dtype == 'i':
                 # integer
                 dtype = dtype.replace('i','d')
-                header_dtypes.append(int)
+                self.header_dtypes[field] = int
             else:
                 # default to float
-                header_dtypes.append(float)
-            header_records[i] = '{:'+rec[1:]+dtype+'}'
-        self.header_fmt = ''.join(header_records)
+                self.header_dtypes[field] = float
+            try:
+                fieldlen = int(rec[1:].split('.')[0])
+            except ValueError:
+                # default field length (e.g., for logical/boolean)
+                fieldlen = 10
+            self.header_fieldlen[field] = fieldlen
+            self.header_fmt += '{:'+rec[1:]+dtype+'}'
         # load file
         if fname is not None:
             self.read(fname)
@@ -50,8 +113,19 @@ class Report(object):
         line = f.readline()
         if line == '':
             return None
-        else:
-            line = line.split()
-            assert (len(line) == len(header_dtypes))
-        return [dtype(val) for dtype,val in zip(header_dtypes,line)]
+        data = []
+        for key in self.header_dtypes.keys():
+            dtype = self.header_dtypes[key]
+            fieldlen = self.header_fieldlen[key]
+            val = line[:fieldlen].strip()
+            try:
+                data.append(dtype(val))
+            except ValueError:
+                print(key,dtype,val)
+            #print(key, ':', line[:fieldlen],dtype(val))
+            line = line[fieldlen:]
+        return data
 
+#    def _read_data(self,f):
+#        line = f.readline().split()
+#        while not line[0] ==
